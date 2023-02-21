d519951070a36e6f3063455a432dd253dd63fe28
Tomjesus <aeltontj@gmail.com>
2023-02-21 10:08:27 -0300

    Busca inicial no ViaCep

--- exemplo.php
+++ exemplo.php
@@ -4,7 +4,7 @@ require_once  'vendor/autoload.php';
 use Tomjesus\DigitalCep\Search;
 
 $busca = new Search;
-echo 'Digite seu Cep:';
+echo 'Digite seu Cep:'."\n";
 $resultado = $busca->getAddressFromZipcode( fgets(STDIN));
 
 print_r($resultado);
